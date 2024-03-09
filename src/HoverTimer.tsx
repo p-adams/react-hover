88f70227f9709c8f08b26694787e8974856bde6a
Paul Adams <ghusiyy@gmail.com>
2024-03-08 23:36:16 -0800

    render items

--- src/HoverTimer.tsx
+++ src/HoverTimer.tsx
@@ -1,6 +1,23 @@
 function HoverTimer() {
-  const items = [];
-  return <div className="hover-container"></div>;
+  const items: any[] = [{ label: "foo" }, { label: "bar" }, { label: "baz" }];
+  const itemsMappedToColors = items.map((item) => ({
+    ...item,
+    color: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`,
+  }));
+
+  return (
+    <div className="hover-container">
+      <ul>
+        {itemsMappedToColors.map((item) => (
+          <li key={item.label} style={{ backgroundColor: item.color }}>
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default HoverTimer;
